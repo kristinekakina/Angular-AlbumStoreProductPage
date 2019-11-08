fcbd22055b8a1d456bf51962276d298c1f9ac2bf
kristinekakina <52838316+kristinekakina@users.noreply.github.com>
2019-11-08 19:09:16 +0300

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,7 +12,7 @@ private _albumUrl= '../asset/album.json';
   constructor(private _http: Http) {}
 
   getAlbum(id: number){
-return this._http.get(this._albumUrl).map((response) =>response.json());
+return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
    
