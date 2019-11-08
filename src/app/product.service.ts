2189d47bd51e2ea9494da576a1f3f94b03460660
kristinekakina <52838316+kristinekakina@users.noreply.github.com>
2019-11-08 15:20:44 +0300

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,11 +6,15 @@ import 'rxjs/add/operator/map'
 @Injectable()
 export class productService {
   private_albumUrl='../asset/album.json';
+  _http: any;
   constructor(private_http: Http) { }
 
 
   getAlbum(id: number){
     return this._http.get(this._albumUrl).map((response)=>response.json());
 
+  }
+  _albumUrl(_albumUrl: any) {
+    throw new Error("Method not implemented.");
   }
 }
