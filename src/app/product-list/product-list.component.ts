f2ffe81dff892b457bbadae2fc0e8c2ad061b926
kristinekakina <52838316+kristinekakina@users.noreply.github.com>
2019-11-08 20:41:31 +0300

    Update product-list.component.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import { ProductService} from '../product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  products:Products[];
+  products:Product[];
 
   constructor(private_productService:ProductService) { }
 
